ee067a15e7db966b4621618cc96baac0c6d454ea
pmt58 <pmt58@cornell.edu>
2024-09-11 21:30:26 -0400

    analysis of first DoE

--- run_spreading.sh
+++ run_spreading.sh
@@ -2,7 +2,7 @@
 #CA=(10 50)
 #($(seq 10 50))
 #OH=(1e-3 3e-3 1e-2 3e-2 1e-1 3e-1 1e+0 3e+0 1e+1 3e+1 1e+2)
-Beta=(0.1 0.2 0.4 0.6 0.8 1)
+Beta=(0.05 0.1 0.2 0.4 0.6 1 5)
 #for j in ${!CA[@]}; do
 #   for i in ${!OH[@]}; do
 #      echo "Running a simulation with Oh=${OH[$i]} and CA=${CA[$j]}"
