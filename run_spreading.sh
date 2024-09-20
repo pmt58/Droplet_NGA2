2ed1aab300fb7884ad69ed1ff47b3e6243b78f08
pmt58 <pmt58@cornell.edu>
2024-09-20 11:33:38 -0400

    power law scaling cases Oh1

--- run_spreading.sh
+++ run_spreading.sh
@@ -3,7 +3,7 @@
 #($(seq 10 50))
 #OH=(1e-3 3e-3 1e-2 3e-2 1e-1 3e-1 1e+0 3e+0 1e+1 3e+1 1e+2)
 
-Beta=(0.1 0.5 1)
+Beta=(0.06)
 G=1
 
 #for j in ${!CA[@]}; do
@@ -16,7 +16,7 @@ G=1
 # Just one test run with Oh=1e-3
 for j in ${!Beta[@]}; do
    echo "Running a simulation with Beta=${Beta[$j]} and Gravity=${G}"
-   mpiexec -n 4 ./nga.dp.gnu.opt.mpi.exe -i input --Beta=${Beta[$j]} --Gravity=${G} >> result.txt
-   mkdir ./monitor_${Beta[$j]}_g${G}
-   mv ./monitor/* ./monitor_${Beta[$j]}_g${G}/
+   mpiexec -n 4 ./nga.dp.gnu.opt.mpi.exe -i input --Beta=${Beta[$j]} >> result.txt
+   mkdir ./monitor2_${Beta[$j]}
+   mv ./monitor/* ./monitor2_${Beta[$j]}/
 done
