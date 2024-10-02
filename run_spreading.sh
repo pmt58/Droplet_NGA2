4eda2bc28b5474ff26ae422a7127df2f6dedc30e
pmt58 <pmt58@cornell.edu>
2024-10-02 13:50:32 -0400

    multirun setup for Contact line methodology case study

--- run_spreading.sh
+++ run_spreading.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
+
 #CA=(10 50)
 #($(seq 10 50))
-#OH=(1e-3 3e-3 1e-2 3e-2 1e-1 3e-1 1e+0 3e+0 1e+1 3e+1 1e+2)
+OH=(1e-3 1e-1)
+CL=(0 1)
+#Beta=(0.06)
+#G=(0)
 
-Beta=(0.06)
-G=1
-
-#for j in ${!CA[@]}; do
-#   for i in ${!OH[@]}; do
-#      echo "Running a simulation with Oh=${OH[$i]} and CA=${CA[$j]}"
-#      mpiexec -n 4 ./nga.dp.gnu.opt.mpi.exe -i input  --Oh=${OH[$i]} --CA=${CA[$j]} >> result_${CA[$j]}.txt
-#   done
-#done
-
-# Just one test run with Oh=1e-3
-for j in ${!Beta[@]}; do
-   echo "Running a simulation with Beta=${Beta[$j]} and Gravity=${G}"
-   mpiexec -n 4 ./nga.dp.gnu.opt.mpi.exe -i input --Beta=${Beta[$j]} >> result.txt
-   mkdir ./monitor2_${Beta[$j]}
-   mv ./monitor/* ./monitor2_${Beta[$j]}/
+# Iteratively test: High/Low Oh : CL model type : Mesh Resolution
+for o in ${!OH[@]}; do
+   for c in ${!CL[@]}; do
+         echo "Running a simulation with Oh=${OH[$o]} CL=${CL[$c]}"
+         mpiexec -n 4 ./nga.dp.gnu.opt.mpi.exe -i input --Oh=${OH[$o]} --CLsolver=${CL[$c]} >> result.txt
+         mkdir ./data_${o}_${CL[$c]}
+         mv ./monitor/* ./data_${o}_${CL[$c]}/
+   done
 done
