2f60c996c1a474ac5a1950e800dc46753dae349b
pmt58 <pmt58@cornell.edu>
2024-10-03 21:37:59 -0400

    exp with droplet spreading consistency under changing grid resolution

--- run_spreading.sh
+++ run_spreading.sh
@@ -2,17 +2,20 @@
 
 #CA=(10 50)
 #($(seq 10 50))
-OH=(1e-3 1e-1)
-CL=(0 1)
+OH=(0.3) #(1e-3 1e-1)
+CL=(2) #1 2)
+Nele=(20 40 60)
 #Beta=(0.06)
 #G=(0)
 
 # Iteratively test: High/Low Oh : CL model type : Mesh Resolution
 for o in ${!OH[@]}; do
    for c in ${!CL[@]}; do
-         echo "Running a simulation with Oh=${OH[$o]} CL=${CL[$c]}"
-         mpiexec -n 4 ./nga.dp.gnu.opt.mpi.exe -i input --Oh=${OH[$o]} --CLsolver=${CL[$c]} >> result.txt
-         mkdir ./data_${o}_${CL[$c]}
-         mv ./monitor/* ./data_${o}_${CL[$c]}/
+      for n in ${!Nele[@]}; do
+         echo "Running a simulation with Oh=${OH[$o]} CL=${CL[$c]} N=${Nele[$n]}"
+         mpiexec -n 4 ./nga.dp.gnu.opt.mpi.exe -i input --Oh=${OH[$o]} --CLsolver=${CL[$c]} --nx=${Nele[$n]} --ny=${Nele[$n]} --nz=${Nele[$n]} >> result.txt
+         mkdir ./data3_${CL[$c]}_${o}_${n}
+         mv ./monitor/* ./data3_${CL[$c]}_${o}_${n}/
+      done
    done
 done
