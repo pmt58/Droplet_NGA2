c4beb3a57ee6e4797374f13c53c76dc7896380e3
pmt58 <pmt58@cornell.edu>
2024-10-05 13:33:35 -0400

    test cases with fixed grad alpha

--- run_spreading.sh
+++ run_spreading.sh
@@ -3,7 +3,7 @@
 #CA=(10 50)
 #($(seq 10 50))
 OH=(0.3) #(1e-3 1e-1)
-CL=(2) #1 2)
+CL=(1 2) #1 2)
 Nele=(20 25 30)
 #Beta=(0.06)
 #G=(0)
@@ -14,8 +14,8 @@ for o in ${!OH[@]}; do
       for n in ${!Nele[@]}; do
          echo "Running a simulation with Oh=${OH[$o]} CL=${CL[$c]} N=${Nele[$n]}"
          mpiexec -n 4 ./nga.dp.gnu.opt.mpi.exe -i input --Oh=${OH[$o]} --CLsolver=${CL[$c]} --nx=${Nele[$n]} --ny=${Nele[$n]} --nz=${Nele[$n]} >> result.txt
-         mkdir ./data1_${CL[$c]}_${o}_${n}
-         mv ./monitor/* ./data1_${CL[$c]}_${o}_${n}/
+         mkdir ./data_${CL[$c]}_${o}_${n}
+         mv ./monitor/* ./data_${CL[$c]}_${o}_${n}/
       done
    done
 done
