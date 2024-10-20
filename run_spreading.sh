6e57470df217c583732077e4cec82e07ec446fd7
pmt58 <pmt58@cornell.edu>
2024-10-20 17:55:54 -0400

    push solver config from new cases

--- run_spreading.sh
+++ run_spreading.sh
@@ -2,9 +2,9 @@
 
 #CA=(10 50)
 #($(seq 10 50))
-OH=(1) #(1e-3 1e-1)
-CL=(1)
-Nele=(20 30 40)
+OH=(0.1) #(1e-3 1e-1)
+CL=(2)
+Nele=(16 32 64)
 #Beta=(0.06)
 #G=(0)
 
@@ -13,9 +13,9 @@ for o in ${!OH[@]}; do
    for c in ${!CL[@]}; do
       for n in ${!Nele[@]}; do
          echo "Running a simulation with Oh=${OH[$o]} CL=${CL[$c]} N=${Nele[$n]}"
-         mpiexec -n 4 ./nga.dp.gnu.opt.mpi.exe -i input --Oh=${OH[$o]} --CLsolver=${CL[$c]} --nx=${Nele[$n]} --ny=${Nele[$n]} --nz=${Nele[$n]} >> result.txt
-         mkdir ./data3_${CL[$c]}_${o}_${n}
-         mv ./monitor/* ./data3_${CL[$c]}_${o}_${n}/
+         mpiexec -n 6 ./nga.dp.gnu.opt.mpi.exe -i input --Oh=${OH[$o]} --CLsolver=${CL[$c]} --nx=${Nele[$n]} --ny=${Nele[$n]} --nz=${Nele[$n]} >> result.txt
+         mkdir ./data1_${CL[$c]}_${o}_${n}
+         mv ./monitor/* ./data1_${CL[$c]}_${o}_${n}/
       done
    done
 done
