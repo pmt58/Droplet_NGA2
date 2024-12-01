e9372215728030f800a3df87e0ba181a580246be
pmt58 <pmt58@cornell.edu>
2024-12-01 17:22:29 -0500

    revert changes

--- src/simulation.f90
+++ src/simulation.f90
@@ -742,9 +742,9 @@ contains
                      &      abs(calculateVolume(vf%interface_polygon(1,i  ,j,k)))*dot_product(calculateNormal(vf%interface_polygon(1,i  ,j,k)),nw))/mysurf
                      ! SGS shear x
                      SGSresU(i,j,k)=-(2*fs%U(i,j,k)*fs%visc_l*my_log(L_slip*fs%cfg%dx(i))/(tan_contact_angle))*&
-                     & abs(sum(fs%divu_x(:,i,j,k)*vf%VF(i-1:i,j,k)))/fs%cfg%dx(i)
+                     & abs(sum(fs%divu_x(:,i,j,k)*vf%VF(i-1:i,j,k)))/(fs%cfg%dx(i))!**2.0_WP)
                      ! Apply x CL youngs force
-                     CLresU(i,j,k)=fs%sigma*(mycos-cos_contact_angle)*sum(fs%divu_x(:,i,j,k)*vf%VF(i-1:i,j,k))/fs%cfg%dy(j)!**2.0_WP
+                     CLresU(i,j,k)=fs%sigma*(mycos-cos_contact_angle)*sum(fs%divu_x(:,i,j,k)*vf%VF(i-1:i,j,k))/(fs%cfg%dy(j))!**2.0_WP)
                      ! Pressure based CL model
                      ! CL_Pjz(i,j,k)=CL_Pjz(i,j,k)+fs%sigma*(mycos-cos_contact_angle)*abs(sum(fs%divu_x(:,i,j,k)*vf%VF(i-1:i,j,k)))*fs%cfg%dy(j)
                   endif
@@ -756,9 +756,9 @@ contains
                      &      abs(calculateVolume(vf%interface_polygon(1,i,j,k  )))*dot_product(calculateNormal(vf%interface_polygon(1,i,j,k  )),nw))/mysurf
                      ! SGS Shear z
                      SGSresW(i,j,k)=-(2*fs%W(i,j,k)*fs%visc_l*my_log(L_slip*fs%cfg%dz(k))/(tan_contact_angle))*&
-                     & abs(sum(fs%divw_z(:,i,j,k)*vf%VF(i,j,k-1:k)))/fs%cfg%dz(i)
+                     & abs(sum(fs%divw_z(:,i,j,k)*vf%VF(i,j,k-1:k)))/(fs%cfg%dz(i))!**2.0_WP)
                      ! Apply z CL youngs force
-                     CLresW(i,j,k)=fs%sigma*(mycos-cos_contact_angle)*sum(fs%divw_z(:,i,j,k)*vf%VF(i,j,k-1:k))/fs%cfg%dy(j)!**2.0_WP
+                     CLresW(i,j,k)=fs%sigma*(mycos-cos_contact_angle)*sum(fs%divw_z(:,i,j,k)*vf%VF(i,j,k-1:k))/(fs%cfg%dy(j))!**2.0_WP)
                      ! Pressure based CL model
                      ! CL_Pjz(i,j,k)=CL_Pjz(i,j,k)+fs%sigma*(mycos-cos_contact_angle)*abs(sum(fs%divw_z(:,i,j,k)*vf%VF(i,j,k-1:k)))*fs%cfg%dy(j)
                   endif
