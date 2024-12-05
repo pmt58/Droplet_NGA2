09d5b71a81d6c3500af47815a3153d64638437fe
pmt58 <pmt58@cornell.edu>
2024-12-04 20:15:14 -0500

    high viscosity case study

--- run_spreading.sh
+++ run_spreading.sh
@@ -2,9 +2,9 @@
 
 #CA=(10 50)
 #($(seq 10 50))
-OH=(0.1) #(1e-3 1e-1)
-CL=(0 1 2)
-Nele=(16 32 45)
+OH=(0.5) #(1e-3 1e-1)
+CL=(0 2)
+Nele=(70)
 #Beta=(0.06)
 #G=(0)
 
@@ -13,7 +13,7 @@ for o in ${!OH[@]}; do
    for c in ${!CL[@]}; do
       for n in ${!Nele[@]}; do
          echo "Running a simulation with Oh=${OH[$o]} CL=${CL[$c]} N=${Nele[$n]}"
-         mpiexec -n 4 ./nga.dp.gnu.opt.mpi.exe -i input --Oh=${OH[$o]} --CLsolver=${CL[$c]} --nx=${Nele[$n]} --ny=${Nele[$n]} --nz=${Nele[$n]} >> result.txt
+         mpiexec -n 8 -oversubscribe ./nga.dp.gnu.opt.mpi.exe -i input --Oh=${OH[$o]} --CLsolver=${CL[$c]} --nx=${Nele[$n]} --ny=${Nele[$n]} --nz=${Nele[$n]} >> result.txt
          mkdir ./data00_${CL[$c]}_${o}_${n}
          mv ./monitor/* ./data00_${CL[$c]}_${o}_${n}/
       done
