9c5539643e0e656ad7848a3a84fa6f289a71aba6
pmt58 <pmt58@cornell.edu>
2024-12-05 21:41:08 -0500

    Oh=0.05 case study

--- run_spreading.sh
+++ run_spreading.sh
@@ -2,9 +2,9 @@
 
 #CA=(10 50)
 #($(seq 10 50))
-OH=(0.5) #(1e-3 1e-1)
+OH=(0.05) #(1e-3 1e-1)
 CL=(0 2)
-Nele=(70)
+Nele=(32 45)
 #Beta=(0.06)
 #G=(0)
 
@@ -14,8 +14,8 @@ for o in ${!OH[@]}; do
       for n in ${!Nele[@]}; do
          echo "Running a simulation with Oh=${OH[$o]} CL=${CL[$c]} N=${Nele[$n]}"
          mpiexec -n 8 -oversubscribe ./nga.dp.gnu.opt.mpi.exe -i input --Oh=${OH[$o]} --CLsolver=${CL[$c]} --nx=${Nele[$n]} --ny=${Nele[$n]} --nz=${Nele[$n]} >> result.txt
-         mkdir ./data00_${CL[$c]}_${o}_${n}
-         mv ./monitor/* ./data00_${CL[$c]}_${o}_${n}/
+         mkdir ./data000_${CL[$c]}_${o}_${n}
+         mv ./monitor/* ./data000_${CL[$c]}_${o}_${n}/
       done
    done
 done
